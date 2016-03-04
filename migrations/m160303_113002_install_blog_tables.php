e8d07d6816c693e0f134b6728eb12fe0623ea805
FedosimovSV <fedosimovsv@makc.ru>
2016-03-04 11:05:53 +0300

    add fonts

---
+++ migrations/m160303_113002_install_blog_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160303_113002_install_blog_tables extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160303_113002_install_blog_tables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
