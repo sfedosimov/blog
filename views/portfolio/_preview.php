bc2cea4320588534e7932b2828c1a4a731586b04
FedosimovSV <fedosimovsv@makc.ru>
2016-03-24 16:43:40 +0300

    fix portfolio separator

--- views/portfolio/_preview.php
+++ views/portfolio/_preview.php
@@ -7,10 +7,10 @@
 
 <?= Html::a('<h2 class="name">'.$model->title.'</h2><div class="cover-wrapper"><img src="'. \Yii::getAlias($model->img_preview) .'" class="cover"></div>', ['view', 'id' => $model->id], ['class' => 'portfolio-list-item']) ?>
 
-<?php if ($index > 2 && $index % 2 === 0): ?>
-    <div class="separator-3"></div>
+<?php if ($index % 2 === 1): ?>
+    <div class="separator-2"></div>
 <?php endif ?>
-<?php if ($index > 3 && $index % 3 === 0): ?>
+<?php if ($index % 3 === 2): ?>
     <div class="separator-3"></div>
 <?php endif ?>
 
